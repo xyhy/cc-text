c60e945020e24a6d7eb5635ef7b74ebc9c197bef
108062213 <xyhy108062213@gapp.nthu.edu.tw>
2022-06-13 00:10:08 +0800

    awstest file

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,34 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awstest91d9e1b8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "S3Triggerca45b667": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "s318c2d0cb": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "api52979a20": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
